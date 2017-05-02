203a7c70fc58c7aa82de279cadbb11f3ecab28b3
eshwar rachala <eshwar.appdev@gmail.com>
2017-05-01 23:19:36 -0400

    updated external modules

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { BarchartGroupedComponent } from './components/grouped/bar-group.compone
 import { BarchartComponent } from './components/bar-chart/bar-chart.component';
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { D3Service } from './d3/d3.service';
+import { D3Service } from './services/d3.service';
 // import { LinechartComponent } from './line-chart/line-chart.component';
 
 export * from './services/d3.service';
