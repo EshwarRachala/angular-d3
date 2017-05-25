8de69385b6ce4a746c64e66a3746bc68edb389e9
eshwar rachala <eshwar.appdev@gmail.com>
2017-05-25 00:44:27 -0400

    added pie chart

---
+++ src/components/pie-chart.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit, ViewEncapsulation, ViewChild, ElementRef, OnChanges, Input } from '@angular/core';
+import nv from 'nvd3';
+import * as d3 from 'd3';
+
+@Component({
+    selector: 'pie-chart',
+    template: '<svg id="chart"></svg>',
+    encapsulation: ViewEncapsulation.None
+})
+export class PieChartComponent implements OnInit, OnChanges {
+    chart: any;
+    @Input() data: any;
+    @Input() height: number;
+
+    constructor() { }
+
+    ngOnInit() {
+        this.createChart();
+        if (this.data) {
+            this.updateChart();
+        }
+    }
+
+    ngOnChanges() {
+        if (this.chart) {
+            this.updateChart();
+        }
+    }
+
+    createChart() {
+
+        this.chart = nv.models.pieChart()
+            .x(function (d) { return d.label })
+            .y(function (d) { return d.value })
+            .showLabels(true)
+            .labelThreshold(.05);
+    }
+
+    updateChart() {
+        d3.select('#chart')
+            .attr('height', this.height)
+            .datum(this.data)
+            .transition()
+            .duration(350)
+            .call(this.chart);
+
+        nv.utils.windowResize(this.chart.update);
+
+    }
+
+}
--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,20 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ChartService } from './nv-d3.service';
+import { PieChartComponent } from './components/pie-chart.component';
 export * from './nv-d3.service';
+export * from './components/pie-chart.component';
+
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
+    PieChartComponent
   ],
   exports: [
+    PieChartComponent
   ],
   providers: [ChartService]
 })
