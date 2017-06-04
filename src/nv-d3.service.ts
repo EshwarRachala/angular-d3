8d217f9def2e98b7c39b0f74ea5646c911310110
eshwar rachala <eshwar.appdev@gmail.com>
2017-06-04 06:18:35 -0400

    updated

--- src/nv-d3.service.ts
+++ src/nv-d3.service.ts
@@ -2,12 +2,16 @@ import { Injectable } from '@angular/core';
 import * as nv from 'nvd3';
 import * as d3 from 'd3';
 
+export type D3 = typeof d3;
+export type NV = typeof nv;
+
+
 @Injectable()
 export class ChartService {
 
   constructor() { }
 
-  getnvD3() {
+  getnvD3(): NV {
     return nv;
   }
 
